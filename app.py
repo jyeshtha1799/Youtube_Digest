d6e6767880b3c8b87f9530f83ebad7cc2557cfe4
jyeshtha1799 <114448454+jyeshtha1799@users.noreply.github.com>
2024-10-18 12:39:11 -0400

    getting transcript details

--- app.py
+++ app.py
@@ -24,3 +24,21 @@ genai.configure(api_key=api_key)
 prompt = """You are a YouTube video summarizer. You will be taking the transcript text
 and summarizing the entire video and providing the important summary in points
 within 250 words. Please provide the summary of the text given here:  """
+
+# Define the function to extract transcript details
+def extract_transcript_details(youtube_video_url, language_code="en"):
+    try:
+        video_id = youtube_video_url.split("=")[1]
+        transcript_text = YouTubeTranscriptApi.get_transcript(video_id, languages=[language_code])
+        transcript = " ".join([i["text"] for i in transcript_text])
+        return transcript
+    except NoTranscriptFound:
+        st.error(f"No transcripts found in the requested language: {language_code}. Please try a different language.")
+        return None
+    except TranscriptsDisabled:
+        st.error("Transcripts are disabled for this video.")
+        return None
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+        return None
+
